446771258c8ff29e020142bf9845646e7396dae1
Milica Jovanovic <milica1205@gmail.com>
2025-07-31 16:24:06 -0400

    @Lazy annotation

--- src/main/java/com/springboot/demo/dev_spring_boot/common/CricketCoach.java
+++ src/main/java/com/springboot/demo/dev_spring_boot/common/CricketCoach.java
@@ -1,10 +1,15 @@
 package com.springboot.demo.dev_spring_boot.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class CricketCoach implements Coach {
 
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/springboot/demo/dev_spring_boot/common/TrackCoach.java
+++ src/main/java/com/springboot/demo/dev_spring_boot/common/TrackCoach.java
@@ -1,11 +1,16 @@
 package com.springboot.demo.dev_spring_boot.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
 public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
